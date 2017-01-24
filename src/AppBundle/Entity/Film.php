9ed2cc0cdaf69dce44a8d7bdf4921756bf08357a
Formation <Formation@10.100.200.99>
2017-01-24 10:13:54 +0100

    Version initiale

---
+++ src/AppBundle/Entity/Film.php
@@ -0,0 +1,158 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Film
+ *
+ * @ORM\Table(name="film")
+ * @ORM\Entity(repositoryClass="AppBundle\Repository\FilmRepository")
+ */
+class Film
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="titre", type="string", length=64)
+     */
+    private $titre;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="synopsis", type="text", nullable=true)
+     */
+    private $synopsis;
+
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="duree", type="integer", nullable=true)
+     */
+    private $duree;
+
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="anneeProd", type="integer", nullable=true)
+     */
+    private $anneeProd;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set titre
+     *
+     * @param string $titre
+     *
+     * @return Film
+     */
+    public function setTitre($titre)
+    {
+        $this->titre = $titre;
+
+        return $this;
+    }
+
+    /**
+     * Get titre
+     *
+     * @return string
+     */
+    public function getTitre()
+    {
+        return $this->titre;
+    }
+
+    /**
+     * Set synopsis
+     *
+     * @param string $synopsis
+     *
+     * @return Film
+     */
+    public function setSynopsis($synopsis)
+    {
+        $this->synopsis = $synopsis;
+
+        return $this;
+    }
+
+    /**
+     * Get synopsis
+     *
+     * @return string
+     */
+    public function getSynopsis()
+    {
+        return $this->synopsis;
+    }
+
+    /**
+     * Set duree
+     *
+     * @param integer $duree
+     *
+     * @return Film
+     */
+    public function setDuree($duree)
+    {
+        $this->duree = $duree;
+
+        return $this;
+    }
+
+    /**
+     * Get duree
+     *
+     * @return int
+     */
+    public function getDuree()
+    {
+        return $this->duree;
+    }
+
+    /**
+     * Set anneeProd
+     *
+     * @param integer $anneeProd
+     *
+     * @return Film
+     */
+    public function setAnneeProd($anneeProd)
+    {
+        $this->anneeProd = $anneeProd;
+
+        return $this;
+    }
+
+    /**
+     * Get anneeProd
+     *
+     * @return int
+     */
+    public function getAnneeProd()
+    {
+        return $this->anneeProd;
+    }
+}
